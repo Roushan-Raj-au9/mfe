29bd125bbc6b35c7644360aa63fab6a34e96e7de
Roushan-Raj-fp <roushan.raj@firstprinciples.io>
2023-08-19 19:48:24 +0530

    randomized production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 
 const App = () => {
-    return(
-        <div>
-            <h1>Hi Theres!</h1>
-            <MarketingApp />
-        </div>
+    return (
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName} >
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
+        </BrowserRouter>
     )
 }
 
